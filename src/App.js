c72b84420c11bde0c85ac52f5a110f568dccbe25
Andrew Hunn <ahunn@umich.edu>
2020-01-04 15:55:58 -0500

    Update menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
 
 function App() {
   return (
     <div>
-    <HomePage></HomePage>
+    <Switch>
+      <Route exact path='/' component={HomePage}/>
+      <Route path='/hats' component={HomePage}/>
+    </Switch>
     </div>
   );
 }
