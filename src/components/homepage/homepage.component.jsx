e4e47f1ac796f785db77cf6f75bf99d8b031b2e1
Andrew Hunn <ahunn@umich.edu>
2020-01-04 14:12:41 -0500

    Create and style homepage component

---
+++ src/components/homepage/homepage.component.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './homepage.styles.scss'
+const HomePage = () => (
+    <div className = 'homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div class='content'>
+                    <h1 className='title'>Title</h1>
+                    <span class='subtitle'>Subtitle</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div class='content'>
+                    <h1 className='title'>Title</h1>
+                    <span class='subtitle'>Subtitle</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div class='content'>
+                    <h1 className='title'>Title</h1>
+                    <span class='subtitle'>Subtitle</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div class='content'>
+                    <h1 className='title'>Title</h1>
+                    <span class='subtitle'>Subtitle</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div class='content'>
+                    <h1 className='title'>Title</h1>
+                    <span class='subtitle'>Subtitle</span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+export default HomePage;
